f97f723aae811c59f7cd5f8cc68fea943909eefc
proelbtn <contact@proelbtn.com>
2021-01-01 04:16:39 +0900

    finished "VGA Text Mode"

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
+
     loop {}
 }
